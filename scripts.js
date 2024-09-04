d4e03fda3dae1bdbcbc46cb50e2ce8cdf9033802
mishrasid606 <mishrasid606@gmail.com>
2024-09-04 15:24:22 +0530

    first commit

---
+++ scripts.js
@@ -0,0 +1,6 @@
+// Custom JavaScript
+
+// Example of a basic script: Add any interactive functionality here
+document.addEventListener('DOMContentLoaded', () => {
+    console.log('Document loaded and ready!');
+});
